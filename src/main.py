c96ee99ef48a91127a6ca868c896e2b4624077ad
jatin <user.name>
2025-05-20 13:33:16 +0530

    having problem in alembic that i fix it

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from src.routes import auth,user_route,roles_route,leave_route
+from fastapi.middleware.cors import CORSMiddleware  # ✅ Import CORS
+
 
 
 app = FastAPI(
@@ -7,6 +9,22 @@ app = FastAPI(
     version="v0", 
     description="A template for FastAPI projects",
 )
+
+# ✅ Add CORS Middleware
+origins = [
+    "http://localhost:3000",   # React frontend
+    "http://127.0.0.1:3000"
+    # You can also add other domains if needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,           # Domains allowed
+    allow_credentials=True,
+    allow_methods=["*"],             # Allow all HTTP methods
+    allow_headers=["*"],             # Allow all headers
+)
+
 
 app.include_router(auth.router)
 app.include_router(user_route.router)
